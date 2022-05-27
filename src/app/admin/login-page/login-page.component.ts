b97d98908dd2ad09f33fe0f7fcc73a35e9ba9e1c
Denis Pshenichny <denispshenichny@yandex.ru>
2022-05-27 16:49:05 +0300

    admin panel

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {IUser} from "../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  form!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    })
   }
 
+  submit() {
+    if (this.form.invalid)
+      return;
+
+    const user: IUser = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+  }
 }
