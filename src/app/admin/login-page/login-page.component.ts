40cff7f05e2ace1ec26d917e9461420d26d95bdb
Denis Pshenichny <denispshenichny@yandex.ru>
2022-06-03 16:28:19 +0300

    routes protection

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {IUser} from "../shared/interfaces";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -11,10 +11,22 @@ import {Router} from "@angular/router";
 })
 export class LoginPageComponent implements OnInit {
   form!: FormGroup;
+  isRelogin : boolean = false;
 
-  constructor(public auth: AuthService, private router: Router) { }
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['relogin']) {
+        this.isRelogin = true;
+        setTimeout(() => this.isRelogin = false, 5000);
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)])
